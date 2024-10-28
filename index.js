91c34d04e6d631b60d3e4d35a644f36749f9d054
CITS <cits@CITSs-MBP-3.lan>
2024-10-28 17:55:53 -0400

    added dynamic console for DOM and things we did in class today

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let links = document.querySelectorAll("a");
-console.log(links);
+let allLinks = document.querySelectorAll('a');
+
+let aboutContent = document.querySelector('#about-content');
+
+let footerElement = document.querySelector('.footer');
+
+let contentDivs = document.querySelectorAll('.content');
+
+let pElements = document.querySelectorAll('p');
+
+let footerLinks = document.querySelectorAll('.footer a');
+
+console.log('All Links:', allLinks);
+console.log('About Content:', aboutContent);
+console.log('Footer Element:', footerElement);
+console.log('Content Divs:', contentDivs);
+console.log('Paragraph Elements:', pElements);
+console.log('Footer Links:', footerLinks);
+
